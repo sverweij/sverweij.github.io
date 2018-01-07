5b3b20ccb592ff556208822c9bb1440785ae5c15
sverweij <sverweij@users.noreply.github.com>
2018-01-07 16:38:35 +0100

    :wrench: use an img srcset to deliver the correct resolution of the only image on the page
    instead of serving the biggest one

--- .sw-precache-config.js
+++ .sw-precache-config.js
@@ -1,6 +1,8 @@
 module.exports = {
     staticFileGlobs : [
-        'assets/mug.jpg',
+        'assets/mug-64.jpg',
+        'assets/mug-128.jpg',
+        'assets/mug-256.jpg',
         'index.html',
         'favicon.ico',
         'manifest.json'
